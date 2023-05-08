5d2cb51a1dddabc61350000c7bdebe319b869d1f
LiviaBoechat <limaboechatti@gmail.com>
2023-05-08 20:37:20 -0300

    req. 02 ok

--- challenges/desafio2.sql
+++ challenges/desafio2.sql
@@ -1,4 +1,3 @@
-SELECT COUNT(*) AS "Total de Canções" FROM SpotifyClone.cancoes GROUP BY id_cancao;
-SELECT COUNT(*) AS "Total de Artistas" FROM SpotifyClone.artista GROUP BY id_artista; albumalbumalbumalbumalbumalbumalbum
-SELECT COUNT(*) AS "Total de Álbuns" FROM SpotifyClone.album GROUP BY id_album;
-
+SELECT (SELECT COUNT(*) FROM cancoes) AS cancoes,
+(SELECT COUNT(*) FROM artista) AS artistas,
+(SELECT COUNT(*) FROM album) AS albuns;
