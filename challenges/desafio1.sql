93cf5e8e99b758cad228ae0dd5341c90529f3e2b
LiviaBoechat <limaboechatti@gmail.com>
2023-05-08 20:35:18 -0300

    req. 01 ok

--- challenges/desafio1.sql
+++ challenges/desafio1.sql
@@ -112,18 +112,68 @@ INSERT INTO usuarios (id_usuario, nome, idade, data_assinatura) VALUES
 CREATE TABLE IF NOT EXISTS `historico` (
   `usuarios_id_usuario` INT NOT NULL,
   `cancoes_id_cancao` INT NOT NULL,
-  CONSTRAINT PRIMARY KEY(usuarios_id_usuario, cancoes_id_cancao)
+  `data_reproducao` VARCHAR(50) NOT NULL,
+  PRIMARY KEY (`usuarios_id_usuario`, `cancoes_id_cancao`),
+--   CONSTRAINT PRIMARY KEY(usuarios_id_usuario, cancoes_id_cancao)
+  CONSTRAINT `fk_historico_usuarios1`
+    FOREIGN KEY (`usuarios_id_usuario`)
+    REFERENCES `SpotifyClone`.`usuarios` (`id_usuario`),
+  CONSTRAINT `fk_historico_cancoes1`
+    FOREIGN KEY (`cancoes_id_cancao`)
+    REFERENCES `SpotifyClone`.`cancoes` (`id_cancao`)
 )
 ENGINE = InnoDB;
 
-
+INSERT INTO historico (usuarios_id_usuario, cancoes_id_cancao, data_reproducao) VALUES 
+(1, 2, '2020-05-02 05:30:35'),
+(1, 8, '2022-02-28 10:45:55'),  
+(1, 10, '2020-03-06 11:22:33'), 
+(2, 7, '2020-01-02 07:40:33'), 
+(2, 10, '2022-08-05 08:05:17'), 
+(3, 2, '2020-12-05 18:38:30'), 
+(3, 10, '2020-11-13 16:55:13'), 
+(4, 8, '2021-08-15 17:10:10'), 
+(5, 5, '2020-08-06 15:23:43'), 
+(5, 8, '2022-01-09 01:44:33'), 
+(6, 7, '2017-01-24 00:31:17'), 
+(6, 1, '2017-10-12 12:35:20'), 
+(7, 4, '2011-12-15 22:30:49'), 
+(8, 4, '2012-03-17 14:56:41'), 
+(9, 9, '2022-02-24 21:14:22'), 
+(10, 3, '2015-12-13 08:30:22');
 
 CREATE TABLE IF NOT EXISTS `seguindo` (
   `artista_id_artista` INT NOT NULL,
   `usuarios_id_usuario` INT NOT NULL,
-  CONSTRAINT PRIMARY KEY(artista_id_artista, usuarios_id_usuario)
+  PRIMARY KEY (`artista_id_artista`, `usuarios_id_usuario`),
+--   CONSTRAINT PRIMARY KEY(artista_id_artista, usuarios_id_usuario)
+CONSTRAINT `fk_seguindo_artista1`
+    FOREIGN KEY (`artista_id_artista`)
+    REFERENCES `SpotifyClone`.`artista` (`id_artista`),
+CONSTRAINT `fk_seguindo_usuarios1`
+    FOREIGN KEY (`usuarios_id_usuario`)
+    REFERENCES `SpotifyClone`.`usuarios` (`id_usuario`)  
 )
 ENGINE = InnoDB;
+
+INSERT INTO seguindo(artista_id_artista, usuarios_id_usuario) VALUES 
+(1, 1), 
+(1, 2), 
+(1, 6), 
+(2, 1), 
+(2, 3), 
+(2, 10),
+(3, 1), 
+(3, 2), 
+(3, 9), 
+(4, 4), 
+(5, 5), 
+(6, 5), 
+(6, 6), 
+(6, 7);
+
+
+
 
 
 
