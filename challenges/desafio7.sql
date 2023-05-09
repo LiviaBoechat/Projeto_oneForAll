2f8860213ba94d8bd14be0e813eece6da46d2215
LiviaBoechat <limaboechatti@gmail.com>
2023-05-09 18:03:28 -0300

    req. 7

--- challenges/desafio7.sql
+++ challenges/desafio7.sql
@@ -8,4 +8,6 @@ INNER JOIN album AS al ON al.artista_id_artista = a.id_artista
 INNER JOIN seguindo AS s ON s.artista_id_artista = al.artista_id_artista
 
 GROUP BY artista, album
-ORDER BY pessoas_seguidoras DESC, artista.nome ASC, album.nome ASC;
+ORDER BY pessoas_seguidoras DESC, artista ASC, album ASC;
+
+
